eab6ab187de393b0957e82a17484a0437ce25a1b
selahblah <99794936+selahblah@users.noreply.github.com>
2022-04-20 14:21:30 +0900

    Time: 24 ms (54.34%), Space: 13.7 MB (5.00%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        
+        emp = [[1]]
+
+        if rowIndex == 0:
+            return emp[0]
+        else:
+            for i in range(1, rowIndex+2):
+                pas = [0]*i
+                for j in range(len(pas)):        
+                    if j == 0 or j == len(pas)-1:
+                        pas[j] = 1
+                    else:
+                        pas[j] = emp[i-1][j-1]+emp[i-1][j]
+                emp.append(pas)
+
+        emp = emp[1:]
+        
+        return emp[rowIndex]
