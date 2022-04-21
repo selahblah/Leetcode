2366ab91a371955e6bf56a396d93539ae95e8e85
selahblah <99794936+selahblah@users.noreply.github.com>
2022-04-21 15:46:15 +0900

    Time: 383 ms (88.86%), Space: 24 MB (43.33%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        dic = {}
+        for n in nums:
+            if n in dic:
+                return True
+            else:
+                dic[n] = 0
+        return False
+    
+                
+
+        """
+        return len(nums) != len(set(nums))
+        """
+           
+        """
+        for i in range(len(nums)):
+            if nums[i] in nums[i+1:]:
+                return True
+            else:
+                pass
+        return False
+        """
+                
+                
+            
