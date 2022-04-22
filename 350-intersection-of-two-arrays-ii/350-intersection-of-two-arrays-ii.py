d30465d26b23ea349df0d85892479798aea24e3c
selahblah <99794936+selahblah@users.noreply.github.com>
2022-04-22 18:57:06 +0900

    Time: 50 ms (49.27%), Space: 13.6 MB (59.67%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -6,9 +6,28 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        return list((Counter(nums1) & Counter(nums2)).elements())
+        
+        """
+        if not nums1 or not nums2:
+            return []
         
         
+        res = []
         
+        x = collections.Counter(nums1)
+        y = collections.Counter(nums2)
+        
+        for key,value in x.items():
+            if key in y:
+                n = min(value,y[key])
+                for i in range(n):
+                    res.append(key)
+        return res
+        """
+        
+        
+        """
         result = []
         for i in nums1:
             if i in nums2:
@@ -16,6 +35,6 @@ class Solution(object):
                 nums2.remove(i)
                     
         return result
-        
+        """
             
         
