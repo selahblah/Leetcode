faae68076c1199a7f2926a91c96d173139dc809c
selahblah <99794936+selahblah@users.noreply.github.com>
2022-04-26 13:31:16 +0900

    Time: 752 ms (17.84%), Space: 14.9 MB (14.07%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -5,18 +5,18 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
-        """
-        for i in nums:
+        
+        for i in nums[::-1]:
             if i == 0:
                 nums.remove(i)
                 nums.append(0)
-        """     
+           
          
-        
+        """
         for i in range(len(nums))[::-1]:
             if nums[i] == 0:
                 nums.pop(i)
                 nums.append(0)
-                
+        """       
               
                 
