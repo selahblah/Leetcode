af93cb0438b28e30d879b61f262db47cf4edf173
selahblah <99794936+selahblah@users.noreply.github.com>
2022-04-26 22:02:46 +0900

    Time: 79 ms (44.09%), Space: 13.8 MB (19.26%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -11,13 +11,13 @@ class Solution(object):
         start_color = image[sr][sc]
         if start_color == newColor:
             return image
-        
         direc=[(0,0),(-1,0),(0,1),(1,0),(0,-1)]
+        
         row, col = len(image), len(image[0])
         def dfs(x,y):
             for dx, dy in direc:
                 new_x, new_y = x+dx, y+dy
-                if 0<= new_x< row and 0<= new_y< col and image[new_x][new_y] == start_color:
+                if 0 <= new_x < row and 0 <= new_y < col and image[new_x][new_y] == start_color:
                     image[new_x][new_y]=newColor
                     dfs(new_x,new_y)
             return image
