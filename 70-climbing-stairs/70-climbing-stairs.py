5fc9fa99520e7f5341dd08d4b9470ca23c9e38cd
selahblah <99794936+selahblah@users.noreply.github.com>
2022-04-26 17:39:08 +0900

    Time: 20 ms (66.21%), Space: 13.3 MB (63.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -8,12 +8,12 @@ class Solution(object):
         if n<=2: 
             return n
         
-        dp = [0]*(n+1)
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3,n+1):
+        dp = [0]*(n)
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2,n):
             dp[i] = dp[i-1]+dp[i-2]
-        return dp[n]
+        return dp[n-1]
         
         """
         count = 0
