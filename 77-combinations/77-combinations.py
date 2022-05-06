6f71612ca498163de5c0f439d30eb89570c2dd61
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-06 19:39:57 +0900

    Time: 42 ms (100.00%), Space: 14.6 MB (97.18%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        from itertools import combinations
+        
+        return(list(combinations(list(range(1,n+1)),k)))
+        
