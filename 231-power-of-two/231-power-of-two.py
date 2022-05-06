94f21a8b2731f281dd7e7aae541c05c3f2c0de36
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-06 18:25:12 +0900

    Time: 28 ms (46.39%), Space: 13.3 MB (65.33%) - LeetHub

--- 231-power-of-two/231-power-of-two.py
+++ 231-power-of-two/231-power-of-two.py
@@ -4,12 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        
+        return n>0 and n&(n-1) == 0 
+    
+        """
         if n <= 0:
             return False
         while n %2 ==0:
             n = n/2
         return n == 1
-        
+        """
         
         """
         if n == 1:
