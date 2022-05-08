2647925c521a90ecfaeae6f4a6e00fbaf49753c0
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-08 18:27:48 +0900

    Time: 54 ms (24.49%), Space: 14 MB (21.31%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,20 +1,12 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
-        if len(nums)==0: return 0
-        
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         dp = [0]*(len(nums)+1)
-        
         dp[0] = 0
         dp[1] = nums[0]
         
-        i=1
-        while i < len(nums): 
-            dp[i+1] = max(dp[i],dp[i-1]+nums[i])
+        i = 1
+        while i < len(nums):
+            dp[i+1] = max(nums[i]+dp[i-1], dp[i])
             i += 1
-            
+        
         return dp[len(nums)]
