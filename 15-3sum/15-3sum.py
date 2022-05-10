0207d95768110a7ca6c8d7bb50c00d48fbedcc3f
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-10 21:54:21 +0900

    Time: 795 ms (82.71%), Space: 18.1 MB (73.10%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,42 +1,29 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        res = []
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if nums is None or nums==[0]: return []
+        
         nums.sort()
+        res=[]
         
         for i, a in enumerate(nums):
+            
             if i > 0 and a == nums[i-1]:
                 continue
             
-            left, right = i+1, len(nums)-1
+            left = i+1
+            right = len(nums)-1
+        
             while left < right:
-                threeSum = a + nums[left] + nums[right]
-                if threeSum > 0:
-                    right -= 1
-                elif threeSum < 0:
+                tem = a + nums[left] + nums[right]
+                if tem < 0:
                     left += 1
+                elif tem > 0:
+                    right -= 1
                 else:
                     res.append([a,nums[left],nums[right]])
                     left += 1
                     while nums[left] == nums[left-1] and left < right:
-                        left += 1
-        return res
-        
-        """
-        for i in nums:
-            nums.remove(i)
-            print(nums)
-            while left < right:
-                if nums[left] + nums[right] == -i:
-                    tem.extend([nums[left], nums[right], i])
-                    res.append(tem)
-                elif nums[left] + nums[right] > -i:
-                    right -= 1
-                else:
-                    left += 1
+                        left +=1
                     
         return res
-        """
+                
