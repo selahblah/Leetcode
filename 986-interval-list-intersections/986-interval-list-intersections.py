d770e2267ea6c92cd2dcf8d7755e7770c50b9a60
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-15 18:54:56 +0900

    Time: 243 ms (11.19%), Space: 14.4 MB (35.60%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -1,49 +1,23 @@
-class Solution:
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        
-        a,b = 0,0
-        N,M = len(firstList),len(secondList)
+class Solution(object):
+    def intervalIntersection(self, firstList, secondList):
+        """
+        :type firstList: List[List[int]]
+        :type secondList: List[List[int]]
+        :rtype: List[List[int]]
+        """
         
         res = []
-        while (a<N and b<M):
+        a,b = 0, 0
+        A,B = len(firstList), len(secondList)
+        
+        while a<A and b<B:
             start = max(firstList[a][0],secondList[b][0])
             end = min(firstList[a][1],secondList[b][1])
             
-            if start <= end:
-                res.append([start,end])
-            
+            if start <= end: res.append([start,end])
+                
             if firstList[a][1] < secondList[b][1]:
                 a += 1
-            else:
-                b += 1
-        return res
-                
-        
-        
-        """
-        if not (firstList and secondList): return []
-        left1, left2 = 0, 0
-        
-        res = [[max(firstList[left1][0],secondList[left2][0]), 
-                min(firstList[left1][1],secondList[left2][1])]]
-        prev_end = max(firstList[left1][1],secondList[left2][1])
-        left1, left2 = 1, 1
-        
-        while left1 < len(firstList) and left2 < len(secondList):
-            start_min = min(firstList[left1][0],secondList[left2][0])
-            start_max = max(firstList[left1][0],secondList[left2][0])
-            end_min = min(firstList[left1][1],secondList[left2][1])
+            else: b += 1
             
-            if prev_end > start_min:
-                res[-1][0] = max(start_min, res[-1][0])
-                res[-1][-1] = min(prev_end,end_min)
-            elif prev_end == start_min:
-                res.append([start_min,prev_end])
-            if end_min >= start_max: res.append([start_max,end_min])
-            
-            prev_end = max(firstList[left1][1],secondList[left2][1])
-            left1 += 1
-            left2 += 1
-        
         return res
-        """
