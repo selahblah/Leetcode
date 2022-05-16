a2e83cb42b4cab46c6c4d2970775b0d21c78118a
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-16 11:25:56 +0900

    Time: 82 ms (88.80%), Space: 17.8 MB (54.26%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,28 +1,13 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        lookup = defaultdict(list)
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dic = defaultdict(list)
         
         for s in strs:
             key = "".join(sorted(s))
-            lookup[key].append(s)
+            dic[key].append(s)
         
-        return [l for l in lookup.values()]
-        
-        
-        """
-        from collections import Counter
-        
-        if len(strs)==1 and len(strs[0]) ==0: return [strs]
-        
-        res = [[strs[0]]]
-        for i in range(1,len(strs)):
-            for j in range(len(res)):
-                if Counter(strs[i]) == Counter(res[j][0]):
-                    res[j].append(strs[i])
-                    break
-                elif j < len(res)-1: continue
-                else: res.append([strs[i]])
-        
-        return res
-        """
+        return [l for l in dic.values()]
