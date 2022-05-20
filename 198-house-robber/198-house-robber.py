4444fad5001b36f1d77deddb8b7216a52d14c8e1
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-20 10:52:53 +0900

    Time: 33 ms (20.04%), Space: 13.3 MB (90.46%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,21 +1,14 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        tem1, tem2 = 0, 0
         for n in nums:
-            tem = max(n+rob1, rob2)
-            rob1 = rob2
-            rob2 = tem
-        return tem
-        
-        """
-        dp = [0]*(len(nums)+1)
-        dp[0] = 0
-        dp[1] = nums[0]
-        
-        i = 1
-        while i < len(nums):
-            dp[i+1] = max(nums[i]+dp[i-1], dp[i])
-            i += 1
-        
-        return dp[len(nums)]
-        """
+            res = max(tem1+n, tem2)
+            tem1 = tem2
+            tem2 = res
+            
+        return res
