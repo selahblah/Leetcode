4874772240d35b82bf299d90ab3d55a9b15e2314
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-20 14:06:52 +0900

    Time: 37 ms (70.97%), Space: 13.9 MB (74.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,5 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         import math
+        return int(math.comb(m+n-2,n-1))
+    
+        """
         return int(math.factorial(m+n-2)/(math.factorial(m-1)*math.factorial(n-1)))
-            
+        """
