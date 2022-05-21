347bf7a85081d943a8e97696c5c010d3c4c3e2cd
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-21 17:34:36 +0900

    Time: 28 ms (59.33%), Space: 13.7 MB (33.58%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,19 +1,23 @@
-class Solution:
-    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+class Solution(object):
+    def numberOfArithmeticSlices(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 3: return 0
+        res, i = 0, 0
         
-        if len(nums)<3: return 0
+        while i < len(nums)-1:
+            diff = nums[i+1] - nums[i]
+            tem1, tem2 = 0, 0
+            i += 1
+            while i < len(nums)-1 and nums[i+1] - nums[i] == diff:
+                tem1 += 1
+                tem2 += tem1
+                i += 1
+            res += tem2
         
-        diff = []
-        count, res= 1, 0
-        for i in range(1,len(nums)):
-            diff.append(nums[i]-nums[i-1])
-        for j in range(len(diff)-1):
-            if diff[j] == diff[j+1]: 
-                res += count
-                count += 1
-            else: 
-                count = 1
-            
         return res
+            
         
-        
+            
