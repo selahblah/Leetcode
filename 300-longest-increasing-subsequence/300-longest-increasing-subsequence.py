72122e8328283dbbef5fa27edb7b9c684e735663
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-22 21:32:59 +0900

    Time: 3139 ms (40.77%), Space: 13.9 MB (38.57%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        
+        """
+        
+        
+        N = len(nums)
+        dp = [1]*N
+        
+        for n in range(len(nums)):
+            for i in range(n):
+                if nums[n] > nums[i]:
+                    dp[n] = max(dp[n],dp[i]+1)
+        return max(dp)
+        
+        
+        
+        
+        
+        """
+        if len(nums)==1: return 1
+        
+        res, i = 0, 0
+        
+        for i in range(len(nums)-1):
+            has = [nums[i]]
+            j = i + 1
+            while j < len(nums):
+                if has[-1] < nums[j]:
+                    has.append(nums[j])
+                    j += 1
+                elif len(has) > 1 and has[-2] < nums[j] < has[-1]:
+                    has.pop()
+                    has.append(nums[j])
+                    j += 1
+                else:
+                    j += 1
+                    continue
+            i += 1
+            res = max(res, len(has))      
+        return res
+        """
+                    
