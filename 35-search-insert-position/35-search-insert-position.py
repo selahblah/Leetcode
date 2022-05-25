93cd9f4ff550082914abfa79c31588466b25bdf5
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-25 10:59:54 +0900

    Time: 80 ms (28.51%), Space: 14.8 MB (40.98%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,35 +1,4 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         
-        left = 0
-        right = len(nums)-1
-        
-        while left <= right:
-            mid = (left+right)//2
-            if nums[left] < target:
-                left += 1
-            elif nums[right] > target:
-                right -= 1
-            else:
-                return mid 
-        
-        return left
-                
-        
-        """
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
-            elif target > nums[-1]:
-                return len(nums)
-            elif target < nums[0]:
-                return 0
-            else:
-                if nums[i-1] < target and target < nums[i]:
-                    return i
-        """
+        return bisect_left(nums, target)
