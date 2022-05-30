9cbb5627069989dbb26790945394eeead33574b0
selahblah <99794936+selahblah@users.noreply.github.com>
2022-05-30 19:18:39 +0900

    Time: 18 ms (85.08%), Space: 13.5 MB (70.75%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -5,13 +5,18 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        
-        res = float("-inf")
+        """
         for x in s:
             if not x in t: return False
             else: 
                 ind = t.index(x)
                 t = t[ind+1:]
         return True
+        """
+        stack = list(s)
+        for char in t:
+            if stack and stack[0] == char:
+                stack.pop(0)
+        return stack == []
         
             
