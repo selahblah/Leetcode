79be0477816adaa09921b3fe570fa8fe0541b106
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-06-20 14:40:28 +0900

    Time: 47 ms (55.01%), Space: 14 MB (20.46%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,22 +1,8 @@
-class Solution(object):
-    def isSubsequence(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        """
-        for x in s:
-            if not x in t: return False
-            else: 
-                ind = t.index(x)
-                t = t[ind+1:]
-        return True
-        """
-        stack = list(s)
-        for char in t:
-            if stack and stack[0] == char:
-                stack.pop(0)
-        return stack == []
-        
-            
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s = list(s)
+        if not s: return True
+        for i in t[::-1]:
+            if i == s[-1]: s.pop()
+            if not s: return True
+        return False
