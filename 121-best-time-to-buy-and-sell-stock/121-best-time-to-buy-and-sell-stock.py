c4563509a9e1eec5a51cd46a604b69e564dcf664
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-06-20 16:01:34 +0900

    Time: 1458 ms (18.14%), Space: 22.3 MB (95.42%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -4,24 +4,9 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if len(prices)==1: return 0
-        minn, res = prices[0], 0
-        
-        for sell in prices[1:]:
-            res = max(res, sell-minn)
-            minn = min(minn,sell)
-        return res
-            
-        
-        """
-        heap = []
-        profit = 0
-        
-        for price in prices:
-            heapq.heappush(heap, price)
-            
-            if price-heap[0] > profit:
-                profit = price-heap[0]
-                
+        buy, profit = prices[0], 0
+        for i in prices[1:]:
+            profit = max(profit, i-buy)
+            if i < buy: buy = i
         return profit
-        """
+                
