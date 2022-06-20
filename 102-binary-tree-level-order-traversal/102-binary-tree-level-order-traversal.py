f6adb36e37d648a82f5e9345d77ff7f3d59c10bd
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-06-20 16:43:54 +0900

    Time: 29 ms (58.61%), Space: 14.3 MB (14.05%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -4,8 +4,6 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-
 class Solution(object):
     
     def levelOrder(self, root):
@@ -13,29 +11,12 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        
-        if not root: return root
-        qu, res = deque([root]),[]
-
-        while qu:
-            size = len(qu)
-            tmp=[]
-            while size > 0:
-                node = qu.popleft()
-                tmp.append(node.val)
-                if node.left:
-                    qu.append(node.left)
-                if node.right:
-                    qu.append(node.right)
-                size -= 1
-
-            res.append(tmp)
-        return res
-        
-        
-
+        dic = defaultdict(list)
+        def dfs(root,level):
+            if not root: return
+            dic[level].append(root.val)
+            dfs(root.left,level+1)
+            dfs(root.right,level+1)
+        dfs(root,0)
             
-        
-
-        
-        
+        return [i for i in dic.values()]  
