1fb362d017e283e54a2fe3d6ab83fe34370846a6
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-06-27 15:27:26 +0900

    Time: 91 ms (67.09%), Space: 14.1 MB (73.08%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -6,31 +6,17 @@ class Solution(object):
         :type c: int
         :rtype: List[List[int]]
         """
+        if len(mat)*len(mat[0]) != r*c: return mat
+        new_c = len(mat)*len(mat[0])//r
+        i,res = 0,[]
         
-        N,M = len(mat[0]), len(mat)
-        T = r*c
-        if N*M != T: return mat
+        mat = [x for xs in mat for x in xs]
         
-        output = [[0 for _ in range(c)] for _ in range(r)]
-        k = 0
-        for i in range(M):
-            for j in range(N):
-                output[k//c][k%c] = mat[i][j]
-                k += 1
-                
-        return output
-        
-        """
-        if len(mat) == r: return mat
-        tem = has = res = [] 
-        col = len(mat)*len(mat[0])//r
-        
-        for i in mat:
-            has += i
-        for i in has:
-            if len(tem) == col:
-                res.append(tem)
-                tem = []
-            tem.append(i)
+        while i < len(mat):
+            tem = []
+            while i < len(mat) and len(tem) < new_c:
+                tem.append(mat[i])
+                i += 1
+            res.append(tem)
         return res
-        """
+            
