f1d6a86894b46e422f11bd3aaeac63ea93c9bdae
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-06-27 13:50:03 +0900

    Time: 73 ms (5.59%), Space: 14 MB (17.45%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,61 +1,13 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        
-        out=[]
-        for i in range(numRows):
-            out.append([1])
-            for j in range(1,i):
-                    out[i].append(out[i-1][j-1] + out[i-1][j])
-            if i != 0:
-                out[i].append(1)
-        return out
-                
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        """
-        out=[]
-        for i in range(numRows):
-            out.append([])
-            out[i].append(1)
-            for j in range(1,i):
-                out[i].append(out[i-1][j-1]+out[i-1][j])
-            if i != 0:
-                out[i].append(1)
-        return out
-        """    
-        
-        
-        """
-        emp = [[1]]
-
-        if numRows == 1:
-            return emp
-        else:
-            for i in range(numRows+1):
-                pas = [0]*i
-                for j in range(len(pas)):        
-                    if j == 0 or j == len(pas)-1:
-                        pas[j] = 1
-                    else:
-                        pas[j] = emp[i-1][j-1]+emp[i-1][j]
-                emp.append(pas)
-                
-        return emp[1:]
-        """
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        for i in range(1,numRows):
+            tem = [1]
+            j = 0
+            while len(tem) < i:
+                tem.append(res[-1][j]+res[-1][j+1])
+                j += 1
+            tem.append(1)
+            res.append(tem)
+        return res
+            
