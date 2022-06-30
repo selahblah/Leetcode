2178aff6e86897ed8c2a771f655d77dd5c93e002
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-06-30 16:46:14 +0900

    Time: 536 ms (10.15%), Space: 14.7 MB (78.04%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -5,6 +5,7 @@ class Solution:
         for i in range(k-1,len(s)):
             window[s[i]] = window.get(s[i],0) + 1
             res = max(res, window['a']+window['e']+window['i']+window['o']+window['u'])
+            if res == k: return res
             window[s[i-k+1]] -= 1
         return res
         
