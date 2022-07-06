72926162f884f2d78eeddfaec787238c8f672be5
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-07-06 10:36:30 +0900

    Time: 717 ms (69.16%), Space: 24.7 MB (19.96%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -1,37 +1,9 @@
 class Solution:
-    def increasingTriplet(self, nums):
-        if len(set(nums))<3: return False
-        
-        n = len(nums)
-        first = nums[n-1]
-        second = -(2**31)
-        
-        i = n-2
-        while i>=0:
-            if nums[i] < second:
-                return True
-            if first > nums[i]:
-                second = nums[i]
-            else:
-                first = nums[i]
-            i -= 1
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        if len(nums)<3: return False
+        smallest = mid = float("inf")
+        for n in nums:
+            if n <= smallest: smallest = n
+            elif n <= mid: mid = n
+            else: return True
         return False
-        
-        """
-        s_nums = set(nums)
-        if len(s_nums)<3: return False
-        
-        res = [[nums[0]]]
-        for i in range(1,len(nums)):
-            for j in range(len(res)):
-                if nums[i] > res[j][-1]:
-                    res[j].append(nums[i])
-                    break
-                elif j == len(res)-1: 
-                    res.append([nums[i]])
-                    
-        for li in res:
-            if len(li)>=3: return True
-        return False
-        """
-            
