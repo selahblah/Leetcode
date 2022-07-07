5a8e8ddf5f26efe0f669dc5283b98b9b2a1f4818
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-07-07 15:27:23 +0900

    Time: 766 ms (7.62%), Space: 15.2 MB (33.68%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,23 +1,12 @@
-class Solution(object):
-    def findAnagrams(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
-        i, n = 0, len(p)
-        countW = Counter(p)
-        countS = Counter(s[:n-1])
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
         res = []
-        
-        
-        while i <= len(s)-n:
-            countS[s[i+n-1]] += 1
-            if countS == countW: res.append(i)
-            countS[s[i]] -= 1
-            if countS[s[i]] ==0: countS.pop(s[i])
-            i += 1
-            
+        count = Counter(p)
+        window = Counter(s[:len(p)-1])
+        for i in range(len(p)-1,len(s)):
+            window[s[i]] = window.get(s[i],0) + 1
+            if window == count: res.append(i-len(p)+1)
+            window[s[i-len(p)+1]] -= 1
+            if window[s[i-len(p)+1]] == 0: del(window[s[i-len(p)+1]])
         return res
             
-        
