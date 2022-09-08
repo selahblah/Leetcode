7ef42e3a28d28decdc67ca5ad2aa103d96f25576
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-09-08 11:02:10 +0900

    Time: 107 ms (10.08%), Space: 14.6 MB (98.83%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,5 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        return bisect_left(nums,target)
+        """
         l,r = 0,len(nums)-1
         while l <= r:
             m = l+(r-l)//2
@@ -7,4 +9,5 @@ class Solution:
             elif nums[m] < target: l += 1
             else: return m
         return l
+        """
         
