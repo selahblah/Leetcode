a5da7bf27f287d19c0cc230e3198e97a4e7903c2
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-09-12 02:33:37 +0900

    Time: 64 ms (10.92%), Space: 13.9 MB (17.84%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(1,numRows):
             tem = [1]
             j = 0
-            while j+1 <= i  and len(tem) < i:
+            while len(tem) < i:
                 tem.append(res[-1][j]+res[-1][j+1])
                 j += 1
             tem.append(1)
