cba2c51338125cb3b4eb68e1b17c705e7ab6b891
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-09-15 01:08:26 +0900

    Time: 1517 ms (6.93%), Space: 24.1 MB (5.51%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        l,r = 0,len(height)-1
+        l,r = 0, len(height)-1
         res = 0
         while l<r:
-            res = max(res,(r-l)*min(height[r],height[l]))
-            if height[r]<height[l]: r -= 1
+            res = max((r-l)*min(height[l],height[r]), res)
+            if height[l] > height[r]: r -= 1
             else: l += 1
         return res
