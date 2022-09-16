0b9d9ba249c402f43b3b8653dd2922c2e7ef5424
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-09-16 22:37:20 +0900

    Time: 315 ms (8.44%), Space: 14.4 MB (9.74%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -5,19 +5,12 @@ class Solution(object):
         :type secondList: List[List[int]]
         :rtype: List[List[int]]
         """
-        
         res = []
-        a,b = 0, 0
-        A,B = len(firstList), len(secondList)
-        
-        while a<A and b<B:
-            start = max(firstList[a][0],secondList[b][0])
-            end = min(firstList[a][1],secondList[b][1])
-            
+        i = j = 0
+        while i < len(firstList) and j < len(secondList):
+            start = max(firstList[i][0],secondList[j][0])
+            end = min(firstList[i][1],secondList[j][1])
             if start <= end: res.append([start,end])
-                
-            if firstList[a][1] < secondList[b][1]:
-                a += 1
-            else: b += 1
-            
+            if firstList[i][1] < secondList[j][1]: i += 1
+            else: j += 1
         return res
