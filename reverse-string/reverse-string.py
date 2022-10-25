9995903ce1ab47c045f2ecd31a514b056020e0e8
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-10-25 23:17:00 +0900

    Time: 431 ms, Memory: 18.3 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -3,8 +3,17 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        
+        l,r = 0,len(s)-1
+        while l<r:
+            s[l],s[r] = s[r],s[l]
+            l += 1
+            r -= 1
+        
+        """
         i = len(s)-1
         while i > -1:
             s.insert(len(s)-1-i,s[-1])
             i -= 1
             s.pop()
+        """
