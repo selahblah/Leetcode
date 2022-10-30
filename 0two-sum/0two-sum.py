5a49ded92b1d3b23a1ee517a82139c307950dab2
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-10-30 23:16:11 +0900

    Time: 129 ms, Memory: 15 MB - LeetHub

--- 0two-sum/0two-sum.py
+++ 0two-sum/0two-sum.py
@@ -1,15 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
         dic = {}
         for i in range(len(nums)):
-            if target-nums[i] in dic: return [dic[target-nums[i]],i]
+            if target - nums[i] in dic.keys(): return [dic[target - nums[i]],i]
             else: dic[nums[i]] = i
-        
         """
-        nums.sort()
-        l,r = 0,len(nums)-1
-        while l<r:
-            if nums[l]+nums[r] > target: r-=1
-            elif nums[l]+nums[r] < target: l+=1
-            else: return [l,r]
+        for i in range(len(nums)-1):
+            if target - nums[i] in nums[i+1:]: return [i, nums.index(target - nums[i])]
         """
