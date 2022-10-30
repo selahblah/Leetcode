cc474b0b76747291fe5a24b4832e7ae187992ee0
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-10-30 23:35:23 +0900

    Time: 42 ms, Memory: 13.5 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -1,4 +1,12 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        digits = int("".join([str(n) for n in digits]))+1
-        return [int(d) for d in str(digits)]
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits)):
+            digits[i] = str(digits[i])
+        res = "".join(digits)
+        res = str(int(res) + 1)
+        return [int(n) for n in res]
+        
