36bc0b6f5c0f2d1f8e791362f2037bf9109318a2
Sohoon <99794936+sohoonyoun@users.noreply.github.com>
2022-11-01 23:43:55 +0900

    Time: 1581 ms, Memory: 29 MB - LeetHub

--- increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -1,5 +1,15 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
+        
+        if len(nums)<3: return False
+        smallest = mid = float("inf")
+        for n in nums:
+            if n <= smallest: smallest = n
+            elif n <= mid: mid = n
+            else: return True
+        return False
+        
+        """
         tem = [nums[0]]
         for n in nums[1:]:
             x = bisect_left(tem,n)
@@ -7,3 +17,4 @@ class Solution:
             elif x < len(tem): tem[x] = n
             if len(tem) == 3: return True 
         return False
+        """
